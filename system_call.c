1b672684faf714bd25d507b489519564d1d67df4
kevin cotton <kcurcot@gmail.com>
2024-08-01 20:29:06 -0400

    Create system_call.c

---
+++ system_call.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+int main() {
+  system("echo \"processus 1 \""); 
+// on utilise \" pour avoir un " dans une chaîne délimitée par des "
+  system("sleep 10");
+  system("echo \"processus 2 \""); 
+  system("sleep 5");
+  system("echo \"processus 3 \""); 
+  return 0;
+}
