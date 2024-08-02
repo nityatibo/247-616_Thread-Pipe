dfb7e48b33a8d18faee7c40709702b4856ee203c
currentleak <kcurcot@gmail.com>
2024-08-02 01:17:44 +0000

    minor changes

--- system_call_thread.c
+++ system_call_thread.c
@@ -1,8 +1,8 @@
 #include <stdlib.h>
-
 int main() {
-  system("echo \"processus 1 \""); // on utilise \" pour avoir un " dans une chaîne délimitée par des "
-  system("sleep 10");
+  system("echo \"processus 1 \""); 
+// on utilise \" pour avoir un " dans une chaîne délimitée par des "
+  system("sleep 10 &");
   system("echo \"processus 2 \""); 
   system("sleep 5");
   system("echo \"processus 3 \""); 
